443b7cf132d22491a1c48f8927de419594114519
Mitlond <comp122012016@mail.ru>
2016-09-22 12:07:15 +0300

    Php

---
+++ PhP/index (1).php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<head>
+<title></title>
+</head>
+<body>
+<?php
+$Arraie[0]="Lux";
+$Arraie[1]="In";
+$Arraie[2]="Tenebris";
+$Arraie[3]="In";
+$Arraie[4]="Vino veritas";
+echo $Arraie[0]; 
+echo "<br>";
+echo $Arraie[2];
+?>
+<?php
+// Ассоциативный массив
+
+$A = $Cryger["7"]="sp1";
+$B = $Cryger["8"]="sp2";
+$C = $Cryger["9"]="sp3";
+echo $Cryger($A+$B+$C);
+?>
+</body>
+</html>
---
+++ PhP/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<head>
+<title></title>
+</head>
+<body>
+<?php
+ 
+$count = 5; //Количество выводимых картинок
+ 
+ for($i = $count; $i > 0; $i--) //Выводим картинку пока переменная "count" больше 0
+ 
+ //Выводим картинку с новой строки, дописав в конце "<br>"
+ echo '<img src="http://cdn2.pitchfork.com/longform/379/mastering-1-matmos-quote.gif"><br>';
+ 
+//Закрываем PHP код
+?>
+</body>
+</html>
